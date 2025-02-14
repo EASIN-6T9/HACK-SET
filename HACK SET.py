e8de0d11c961a39db3809e83c51c846e77f6bc32
xyz <shawon22re@gmail.com>
2025-02-14 09:34:08 +0600

    Add files via upload

---
+++ HACK SET.py
@@ -0,0 +1,207 @@
+'''  
+---------------------------------------------------------------------
+THIS TOOL IS MADE FOR EDUCATIONAL PURPOSE. 
+
+ DATE             :  14/02/2025
+ GITHUB         :  EASIN-6T9“mr-xyz”
+ TOOLS NAME: HACK SET  v_0.1
+ -------------------------------------------------------------------'''
+
+
+
+
+#Importing some moduels*
+import os
+import time
+os.system("clear")
+yellow = "\0331;33m"
+print("\033[1;31m=========================================================")
+
+print('''\033[1;33mv_0.1
+    __  _____   ________ __    _____ ____________
+   / / / /   | / ____/ //_/   / ___// ____/_  __/
+  / /_/ / /| |/ /   / ,<      \__ \/ __/   / /   
+ / __  / ___ / /___/ /| |    ___/ / /___  / /       
+/_/ /_/_/  |_\____/_/ |_|   /____/_____/ /_/     
+''')
+
+print("\033[1;31m=========================================================")
+print("\033[1;32m TOOLS OWNER : EASIN-6T9")
+print("\033[1;32m TOOLS NAME  : HACK SET")
+
+print("\033[1;32m GITHUB      : EASIN-6T9\n\n\033[1;35mThis Tool is made for educational purpose ")
+print("\033[1;31m=========================================================")
+p2 = input("ENTER TOOLS PASSWORD: ")
+
+if p2 =="easin":
+    print("Password is currect\nPlease wait a second...")
+    time.sleep(2)
+    print("\033[1;31m=========================================================")
+    print('''\033[1;33m1) TERMUX BASIC SETUP\n2) TERMUX FULL SETUP\n3) CONTACT US\n4) OUR MORE TOOLS\n\033[1;31m=========================================================  ''')
+    usr1 = int(input("Select a option: "))
+    ''' ---------------♠BASIC SETUP♠-------------  '''
+    if usr1 == 1:
+        print("\nIf you have not given storage permission to Termux, then allow the permission.")
+        os.system("termux-setup-storage")
+        time.sleep(4)
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("pkg update -y")
+        os.system("pkg upgrade -y")
+        os.system("pkg install git -y")
+        os.system("pkg install python -y")
+        os.system("pkg install python3")
+        
+        os.system("pkg install golang -y")
+        os.system("pip install requests ")
+        os.system("pip install random  ")
+        os.system("pip install rich")
+        os.system("pip2 install requests")
+        os.system("apt install tmux ")
+        os.system("pip install wget ")
+        os.system("pip install pycurl ")
+        os.system("pip install bs4")
+        os.system("pip2 install bs4")
+        os.system("gem install lolcat")
+        os.system("pkg install python2 ")
+        os.system("pip install lolcat ")
+        os.system("pip install php ")
+        os.system("pip install httpx ")
+        os.system("pip install futures ")
+        os.system("pip install mechanize ")
+        
+        print('''  \033[1;33mBASIC SETUP SUCCESSFULLY DONE••••  ''')
+        '''  ••••♣BASIC SETUP DONE♣•••••   '''
+         
+     #FULL SETUP
+    elif usr1 == 2:
+        print("Please allow the permission if you aren't")
+        os.system("termux-setup-storage")
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("pkg update -y")
+        os.system("pkg upgrade -y")
+        os.system("pkg install git -y")
+        os.system("pkg install python -y")
+        os.system("pkg install python3")
+        
+        os.system("pkg install golang -y")
+        os.system("pip install requests ")
+        os.system("pip install random  ")
+        os.system("pip install rich")
+        os.system("pip2 install requests")
+        os.system("apt install tmux ")
+        os.system("pip install wget ")
+        os.system("pip install pycurl ")
+        os.system("pip install bs4")
+        os.system("pip2 install bs4")
+        os.system("gem install lolcat")
+        os.system("pkg install python2 ")
+        os.system("pip install lolcat ")
+        os.system("pip install php ")
+        os.system("pip install httpx ")
+        os.system("pip install futures ")
+        os.system("pip install mechanize ")
+        os.system("pkg install python-pip")
+        os.system("pkg install ruby")
+        os.system("pkg install help")
+        os.system("pkg install fish")
+        os.system("pkg install dnsutils")
+        os.system("pkg install lua")
+        os.system("pkg install perl")
+        os.system("pkg install ")
+        os.system("pkg install nmap")
+        os.system("pkg install nano")
+        os.system("pkg install parallel")
+        os.system("pkg install clang")
+        os.system("pkg install bash")
+        os.system("pkg install w3m")
+        os.system("pkg install hydra")
+        os.system("pkg install cowsay")
+        os.syatem("pkg install curl")
+        os.system("pkg install tar")
+        os.system("pkg install zip")
+        os.system("pkg install unzip")
+        os.system("pkg install tor")
+        os.system("pkg install net-tools")
+        os.system("pkg install sudo")
+        os.system("pkg install texinfo")
+        os.system("pkg install crunch")
+        os.system("pkg install wireshark")
+        os.system("pkg install wgetrc")
+        os.system("pkg install wcalc")
+        os.system("pkg install vpn")
+        os.system("pkg install openssl")
+        os.system("pkg install openssl-tool")
+        os.system("pkg install bmon")
+        os.system("pkg install unrar")
+        os.system("pkg install toilet")
+        os.system("pkg install proot")
+        os.system("pkg install vim")
+        os.system("pkg install vim-python")
+        os.system("pkg install ired")
+        os.system("pkg install goaccess")
+        os.system("pkg install golang")
+        os.system("pkg install kibi")
+        os.system("pkg install tsu")
+        os.system("pkg install mtools")
+        os.sytem("pkg install file")
+        os.system("pkg install vis")
+        os.system("pkg install pass")
+        os.system("pkg install pick")
+        os.system("pkg install chroot")
+        os.system("pkg install macchanger")
+        os.system("pkg install elixir")
+        os.system("pkg install ninja")
+        os.system("pkg install xmlstarlet")
+        os.system("pkg install swift")
+        os.system("pkg insgall fakeroot")
+        os.system("pkg install texinfo")
+        os.system("pkg install netcat")
+        os.system("pkg install wren")
+        os.system("pkg install gatling")
+        os.system("pkg install cvs")
+        os.system("pkg install ffmpeg")
+        os.system("pkg install screen")
+        os.system("pkg install neofetch")
+        os.system("pkg install mariadb")
+        os.system("pkg install picolisp")
+        os.system("pkg install cmatrix")
+        os.system("pkg install dropbear")
+        os.system("pkg install openssh")
+        os.system("pkg install python-pip")
+        os.system("gem install lolcat")
+        os.system("pip install php")
+        os.system("pip2 install php")
+        os.system("pip install pytube")
+        time.sleep(3)
+        print("\033[1;32mTermux full setup done✓")
+        #_______FULL-SETUP-DONE______#
+        
+        
+        
+        
+        
+        
+        #Contact me on FB
+    elif usr1 == 3:
+        os.system("termux-open-url https://www.facebook.com/Tomar.abba.shawon")
+        #See My Github Profile For More Tools
+    elif usr1 == 4:
+        os.system("termux-open-url https://github.com/EASIN-6T9")
+        
+'''_____________PROGGRAM FINISH_______________ '''
+    
+    
+      
+    
+ 
+
+    
+    
+else:
+    print("Wrong Input Please Try Again.... ")
+    
+
+
+
